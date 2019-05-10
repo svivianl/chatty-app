0ced115367c6c27dc6d34a221dc7da92f653218d
vshibukawa <viviane.shibukawa@gmail.com>
2019-05-10 15:03:30 +0000

    fix notification style

--- client/src/Notification.jsx
+++ client/src/Notification.jsx
@@ -3,9 +3,7 @@ import React, {Component} from 'react';
 class Notification extends Component {
   render() {
     return (
-      <div className="notification">
-        <span className="notification-content">{this.props.message.content}</span>
-      </div>
+      <div className="notification-content">{this.props.message.content}</div>
     );
   }
 }
